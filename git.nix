ab1b58096af1cfb7fe4df8853a16912bdde43a54
Newton Ni <nwtnni@gmail.com>
2024-04-22 10:59:33 -0500

    Factor out git configuration

---
+++ git.nix
@@ -0,0 +1,145 @@
+{ config, lib, ... }:
+
+{
+  home.shellAliases = {
+    g = "git";
+    gs = "git status";
+    ga = "git add";
+    gaa = "git add :/";
+    gap = "git add -p";
+    gc = "git commit";
+    gca = "git commit --amend";
+    gcm = "git commit --message";
+    gcf = "git commit --fixup";
+    gk = "git checkout";
+    gkp = "git checkout -p";
+    gd = "git diff";
+    gds = "DELTA_FEATURES=+side-by-side git diff";
+    gdc = "git diff --cached";
+    gdcs = "DELTA_FEATURES=+side-by-side git diff --cached";
+    gf = "git fetch";
+    gl = "git log";
+    glp = "git log -p";
+    glps = "DELTA_FEATURES=+side-by-side git log -p";
+    gp = "git push";
+    gpf = "git push --force-with-lease";
+    gr = "git reset";
+    gra = "git reset :/";
+    grp = "git reset -p";
+    gt = "git stash";
+    gtp = "git stash pop";
+    gu = "git pull";
+  };
+
+  programs.bash = lib.mkIf config.programs.bash.enable {
+    historyIgnore = [ "gs" ];
+    initExtra = (lib.strings.optionalString config.programs.fzf.enable ''
+      gbc () {
+        git checkout $(git branch --list | fzf)
+      }
+
+      gbd () {
+        git branch -d $(git branch --list | fzf)
+      }
+    '') + ''
+      grbi() {
+        if [[ "$#" -gt 0 ]]; then
+          git rebase -i "HEAD~$1"
+        elif [[ $(git rev-list --root HEAD --count) -le 10 ]]; then
+          git rebase -i --root
+        else
+          git rebase -i "HEAD~10"
+        fi
+      }
+    '';
+  };
+
+  programs.eza = lib.mkIf config.programs.eza.enable {
+    extraOptions = [ "--git" ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Newton Ni";
+    userEmail = "nwtnni@gmail.com";
+
+    # https://jvns.ca/blog/2024/02/16/popular-git-config-options/#rebase-autostash-true
+    extraConfig = {
+      branch.sort = "-committerdate";
+      commit.verbose = true;
+      diff.algorithm = "histogram";
+      diff.colorMoved = "default";
+      diff.colorMovedWS = "allow-indentation-change";
+      fetch.prune = true;
+      fetch.pruneTags = true;
+      merge.conflictStyle = "zdiff3";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      push.followTags = true;
+      rebase.autoSquash = true;
+      rebase.autoStash = true;
+      rerere.enabled = true;
+      tag.sort = "-taggerdate";
+    };
+
+    ignores = [
+      ".DS_Store"
+      ".direnv"
+      ".envrc"
+      "*.tags"
+      "*.swp"
+    ];
+
+    delta = {
+      enable = true;
+      options = {
+        dark = true;
+        hyperlinks = true;
+        line-numbers = true;
+        navigate = true;
+        syntax-theme = lib.optionalAttrs (config.programs.bat.enable) "gruvbox";
+
+        blame-code-style = "syntax";
+        blame-palette = "#161617 #1b1b1d #2a2a2d #3e3e43";
+
+        file-style = "brightwhite";
+        file-decoration-style = "ol ul";
+
+        hunk-header-decoration-style = "#3e3e43 box ul";
+
+        line-numbers-minus-style = "brightred";
+        line-numbers-plus-style = "brightgreen";
+        line-numbers-left-style = "#3e3e43";
+        line-numbers-right-style = "#3e3e43";
+        line-numbers-zero-style = "#57575f";
+
+        minus-style = "brightred black";
+        minus-emph-style = "black red";
+        plus-style = "brightgreen black";
+        plus-emph-style = "black green";
+
+        map-styles = "\
+          bold purple => syntax purple, \
+          bold blue => syntax blue, \
+          bold cyan => syntax cyan, \
+          bold yellow => syntax yellow";
+
+        file-added-label = "added:";
+        file-modified-label = "modified:";
+        file-removed-label = "removed:";
+        file-renamed-label = "renamed:";
+        merge-conflict-begin-symbol = "~";
+        merge-conflict-end-symbol = "~";
+        right-arrow = "-> ";
+
+        merge-conflict-ours-diff-header-style = "blue bold";
+        merge-conflict-ours-diff-header-decoration-style = "#3e3e43 box";
+        merge-conflict-theirs-diff-header-style = "yellow bold";
+        merge-conflict-theirs-diff-header-decoration-style = "#3e3e43 box";
+
+        whitespace-error-style = "black bold";
+        zero-style = "syntax";
+      };
+    };
+  };
+}
--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  imports = [
+    ./git.nix
+  ];
+
   programs.home-manager.enable = true;
 
   home = {
@@ -36,32 +40,6 @@
       vi = "nvim";
       vim = "nvim";
 
-      g = "git";
-      gs = "git status";
-      ga = "git add";
-      gaa = "git add :/";
-      gap = "git add -p";
-      gc = "git commit";
-      gca = "git commit --amend";
-      gh = "git checkout";
-      ghp = "git checkout -p";
-      gd = "git diff";
-      gds = "DELTA_FEATURES=+side-by-side git diff";
-      gdc = "git diff --cached";
-      gdcs = "DELTA_FEATURES=+side-by-side git diff --cached";
-      gf = "git fetch";
-      gl = "git log";
-      glp = "git log -p";
-      glps = "DELTA_FEATURES=+side-by-side git log -p";
-      gp = "git push";
-      gpf = "git push --force-with-lease";
-      gr = "git reset";
-      gra = "git reset :/";
-      grp = "git reset -p";
-      gt = "git stash";
-      gtp = "git stash pop";
-      gu = "git pull";
-
       cc = "cargo check";
       cb = "cargo build";
       cbr = "cargo build --release";
@@ -77,7 +55,7 @@
     enable = true;
     enableVteIntegration = true;
     historyControl = [ "ignoredups" "ignorespace" "erasedups" ];
-    historyIgnore = [ "l" "ls" "p" "gs" "pwd" ];
+    historyIgnore = [ "l" "ls" "p" "pwd" ];
     shellOptions = [
       "autocd"
       "cdspell"
@@ -85,6 +63,7 @@
       "globstar"
       "histappend"
     ];
+
     initExtra = ''
       __prompt_setc() {
         printf "\x1b[38;2;%s;%s;%sm" "$1" "$2" "$3"
@@ -178,20 +157,6 @@
       susto() { systemctl --user stop $(sus); }
       sur() { systemctl --user restart $(sus); }
 
-      gbc () {
-        git checkout $(git branch --list | fzf)
-      }
-
-      gbd () {
-        git branch -d $(git branch --list | fzf)
-      }
-
-      gri() {
-        if [ ! -z "$1" ]; then
-          git rebase -i "HEAD~$1"
-        fi
-      }
-
       bind '"\C-o": "\ec"'
       bind -x '"\C-e": fe'
     '';
@@ -210,7 +175,6 @@
     enable = true;
     extraOptions = [
       "--classify"
-      "--git"
       "--group-directories-first"
       "--header"
       "--icons"
@@ -224,90 +188,6 @@
     defaultCommand = "fd --type f";
     changeDirWidgetCommand = "fd --type d";
     fileWidgetCommand = "fd --type f";
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "Newton Ni";
-    userEmail = "nwtnni@gmail.com";
-    # https://jvns.ca/blog/2024/02/16/popular-git-config-options/#rebase-autostash-true
-    extraConfig = {
-      branch.sort = "-commiterdate";
-      commit.verbose = true;
-      diff.algorithm = "histogram";
-      diff.colorMoved = "default";
-      diff.colorMovedWS = "allow-indentation-change";
-      fetch.prune = true;
-      fetch.pruneTags = true;
-      merge.conflictStyle = "zdiff3";
-      pull.rebase = true;
-      push.autoSetupRemote = true;
-      push.followTags = true;
-      rebase.autoSquash = true;
-      rebase.autoStash = true;
-      rerere.enabled = true;
-      tag.sort = "-taggerdate";
-    };
-
-    ignores = [
-      ".DS_Store"
-      ".direnv"
-      ".envrc"
-      "*.tags"
-      "*.swp"
-    ];
-
-    delta = {
-      enable = true;
-      options = {
-        dark = true;
-        hyperlinks = true;
-        line-numbers = true;
-        navigate = true;
-        syntax-theme = "gruvbox";
-
-        blame-code-style = "syntax";
-        blame-palette = "#161617 #1b1b1d #2a2a2d #3e3e43";
-
-        file-style = "brightwhite";
-        file-decoration-style = "ol ul";
-
-        hunk-header-decoration-style = "#3e3e43 box ul";
-
-        line-numbers-minus-style = "brightred";
-        line-numbers-plus-style = "brightgreen";
-        line-numbers-left-style = "#3e3e43";
-        line-numbers-right-style = "#3e3e43";
-        line-numbers-zero-style = "#57575f";
-
-        minus-style = "brightred black";
-        minus-emph-style = "black red";
-        plus-style = "brightgreen black";
-        plus-emph-style = "black green";
-
-        map-styles = "\
-          bold purple => syntax purple, \
-          bold blue => syntax blue, \
-          bold cyan => syntax cyan, \
-          bold yellow => syntax yellow";
-
-        file-added-label = "added:";
-        file-modified-label = "modified:";
-        file-removed-label = "removed:";
-        file-renamed-label = "renamed:";
-        merge-conflict-begin-symbol = "~";
-        merge-conflict-end-symbol = "~";
-        right-arrow = "-> ";
-
-        merge-conflict-ours-diff-header-style = "blue bold";
-        merge-conflict-ours-diff-header-decoration-style = "#3e3e43 box";
-        merge-conflict-theirs-diff-header-style = "yellow bold";
-        merge-conflict-theirs-diff-header-decoration-style = "#3e3e43 box";
-
-        whitespace-error-style = "black bold";
-        zero-style = "syntax";
-      };
-    };
   };
 
   programs.tmux = {
