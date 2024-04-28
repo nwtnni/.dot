70084a7c60405b8e4af1f894744a05d1c953f07f
Newton Ni <nwtnni@gmail.com>
2024-04-28 16:28:39 -0500

    Add more intuitive split mapping

--- neovim/lua/plugins/telescope-nvim.lua
+++ neovim/lua/plugins/telescope-nvim.lua
@@ -7,9 +7,19 @@ return {
   },
   cmd = "Telescope",
   keys = {
+    { "<SPACE>tt", "<CMD>Telescope<CR>", desc = "Telescope", unique = true },
     { "<SPACE>tf", "<CMD>Telescope find_files<CR>", desc = "Telescope find files", unique = true },
     { "<SPACE>tg", "<CMD>Telescope live_grep<CR>", desc = "Telescope live grep", unique = true },
     { "<SPACE>tb", "<CMD>Telescope oldfiles<CR>", desc = "Telescope oldfiles", unique = true },
+  },
+  opts = {
+    defaults = {
+      mappings = {
+        i = {
+          ["<C-s>"] = "select_horizontal",
+        },
+      },
+    },
   },
   config = function(plugin, opts)
     local telescope = require(plugin.main)
