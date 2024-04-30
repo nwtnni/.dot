865cd3dd540ee56a4ea0aefab6e6c1e6115067ee
Newton Ni <nwtnni@gmail.com>
2024-04-30 16:47:40 -0500

    Format on save

--- neovim/lua/plugins/nvim-lspconfig.lua
+++ neovim/lua/plugins/nvim-lspconfig.lua
@@ -34,10 +34,28 @@ return {
     }
 
     local lspconfig = require(plugin.main)
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      vim.lsp.protocol.make_client_capabilities(),
+      require("cmp_nvim_lsp").default_capabilities()
+    )
+
     for server, configuration in pairs(configurations) do
       configuration.capabilities = capabilities
       lspconfig[server].setup(configuration)
     end
+
+    local pattern = {}
+    for _, ft in ipairs(plugin.ft) do
+      table.insert(pattern, "*." .. ft)
+    end
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("lsp-format", { clear = true }),
+      pattern = pattern,
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
   end,
 }
