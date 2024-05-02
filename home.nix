723a18ef99550bd1f578ecdf571512b3ce6c4001
Newton Ni <nwtnni@gmail.com>
2024-05-02 14:37:22 -0500

    Try to disable side mouse button

--- home.nix
+++ home.nix
@@ -234,6 +234,10 @@
       bindsym Print exec shotman -c output
       bindsym Print+Shift exec shotman -c region
       bindsym Print+Shift+Control exec shotman -c window
+
+      # HACK: disable dying mouse buttons
+      bindsym --input-device=2362:9505:USB_OPTICAL_MOUSE --whole-window BTN_SIDE true
+      focus_follows_mouse no
     '';
   };
 }
