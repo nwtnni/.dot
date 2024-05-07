e9e64762edd4dd4178c369eca8555c43d60ea629
Newton Ni <nwtnni@gmail.com>
2024-05-07 03:17:48 -0500

    Prototype terminal toggle

--- neovim/init.lua
+++ neovim/init.lua
@@ -161,6 +161,7 @@ vim.o.updatetime = 250
 vim.o.mouse = false
 vim.o.splitright = true;
 vim.o.splitbelow = true;
+vim.o.equalalways = false;
 vim.o.virtualedit = "block";
 vim.o.list = true;
 vim.o.listchars = "trail:·";
@@ -183,8 +184,6 @@ local setn = function(source, target) set("n", source, target) end
 local seti = function(source, target) set("i", source, target) end
 
 setn("<CR>", "<CMD>update<CR>")
-setn("j", "gj")
-setn("k", "gk")
 setn("[d", vim.diagnostic.goto_prev)
 setn("]d", vim.diagnostic.goto_next)
 setn("K", vim.lsp.buf.hover)
@@ -194,6 +193,7 @@ setn("<SPACE>s", toggle_status)
 setn("<SPACE>h", toggle_search)
 
 seti("jf", "<ESC>")
+set("t", "jf", "<C-\\><C-n>")
 
 -- Navigation
 vim.o.cursorlineopt = "screenline"
@@ -202,6 +202,8 @@ local navigate = false
 setn("<C-u>", "")
 setn("<C-d>", "")
 setn("<C-o>", "")
+setn("j", "gj")
+setn("k", "gk")
 
 local function update_cursorline(value)
   vim.api.nvim_set_option_value("cursorline", value, { scope = "local", win = 0 })
@@ -209,21 +211,13 @@ end
 
 local function toggle_navigate()
   if navigate then
-    for _, key in pairs({ "gg", "G", "u", "d", "h", "j", "k", "l", "i", "o" }) do
+    for _, key in pairs({ "gg", "G", "u", "d", "h", "j", "k", "l", "i", "o", "q" }) do
       vim.keymap.del("n", key)
     end
 
     setn("j", "gj")
     setn("k", "gk")
   else
-    local snippy = require("snippy")
-
-    -- Avoid colliding with snippets
-    if snippy.can_jump(1) then
-      snippy.next()
-      return
-    end
-
     pcall(vim.keymap.del, "n", "j")
     pcall(vim.keymap.del, "n", "k")
 
@@ -237,14 +231,13 @@ local function toggle_navigate()
     setn("l", "<C-w>l")
     setn("i", "<C-I>")
     setn("o", "<C-O>")
-    setn("w", "<CMD>update<CR>")
     setn("q", "<CMD>quit<CR>")
   end
   navigate = not navigate
   update_cursorline(navigate)
 end
 
-setn("<TAB>", toggle_navigate)
+setn("w", toggle_navigate)
 
 local navigate_group = vim.api.nvim_create_augroup("navigate", { clear = true })
 
@@ -260,8 +253,66 @@ for _, event in ipairs({ "WinLeave", "WinClosed" }) do
   })
 end
 
-vim.api.nvim_create_autocmd("ModeChanged", {
-  group = navigate_group,
-  pattern = "n:*",
-  callback = function() if navigate then toggle_navigate() end end,
+-- vim.api.nvim_create_autocmd("ModeChanged", {
+--   group = navigate_group,
+--   -- Everything except for normal submodes
+--   pattern = { "n:[ivVsSRt!r]*" },
+--   callback = function() if navigate then toggle_navigate() end end,
+-- })
+
+local win_save
+local function toggle_terminal()
+  local win = vim.api.nvim_get_current_win()
+  local term_win = vim.g._term_win
+  local term_buf = vim.g._term_buf
+
+  if term_buf and not vim.api.nvim_buf_is_loaded(term_buf) then
+    pcall(vim.api.nvim_buf_delete, term_buf)
+    term_buf = nil
+  end
+
+  if term_win and not vim.api.nvim_win_is_valid(term_win) then
+    term_win = nil
+  end
+
+  if term_win and term_buf then
+    if win == term_win then
+      if not pcall(vim.api.nvim_set_current_win, win_save) then
+        vim.cmd.wincmd("k")
+      end
+    else
+      win_save = win
+      vim.api.nvim_set_current_win(term_win)
+    end
+    return
+  end
+
+  win_save = win
+  vim.cmd([[botright split]])
+  vim.g._term_win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_height(vim.g._term_win, 10)
+
+  if term_buf then
+    vim.api.nvim_win_set_buf(vim.g._term_win, term_buf)
+    vim.cmd.startinsert()
+  else
+    vim.cmd.terminal()
+    vim.g._term_buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_keymap(vim.g._term_buf, "n", "q", "<CMD>quit<CR>", { silent = true })
+  end
+end
+
+local augroup_term = vim.api.nvim_create_augroup("personal-term", { clear = true })
+vim.api.nvim_create_autocmd({ "WinEnter", "TermOpen" }, {
+  group = augroup_term,
+  pattern = "term://*",
+  command = "startinsert",
 })
+vim.api.nvim_create_autocmd("TermClose", {
+  group = augroup_term,
+  callback = function(event)
+    vim.api.nvim_buf_delete(event.buf, { force = true })
+  end
+})
+
+setn("<TAB>", toggle_terminal)
