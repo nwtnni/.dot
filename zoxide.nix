693ebed068f8399e877986b626e8e25aeafcf5f3
Newton Ni <nwtnni@gmail.com>
2025-07-31 14:29:32 -0500

    Add interactive zoxide function

--- zoxide.nix
+++ zoxide.nix
@@ -3,4 +3,14 @@
   programs.zoxide = {
     enable = true;
   };
+
+  programs.bash.initExtra = ''
+    o() {
+      if [[ -z $1 ]]; then
+        zi
+      else
+        z $1
+      fi
+    }
+  '';
 }
