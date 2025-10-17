db51d66d30a3d3fec9283f3c3c07406cfad50069
Newton Ni <nwtnni@gmail.com>
2025-10-17 15:21:08 -0500

    Support home-manager installation on chameleon

--- home.nix
+++ home.nix
@@ -1,13 +1,25 @@
 { ... }:
+
+let
+  local = true;
+  username = if local then "nwtnni" else "cc";
+in
 {
   imports = [
     ./tui.nix
-    ./gui.nix
-  ];
+  ] ++ (if local then [./gui.nix] else []);
 
   home = {
-    username = "nwtnni";
-    homeDirectory = "/home/nwtnni";
+    inherit username;
+    homeDirectory = "/home/${username}";
     stateVersion = "25.05";
   };
+
+  # Handle home-manager standalone installation after
+  # multi-user nix installation
+  programs.bash.profileExtra = if local then "" else /* bash */ ''
+    if [ -e /home/cc/.nix-profile/etc/profile.d/nix.sh ]; then
+      . /home/cc/.nix-profile/etc/profile.d/nix.sh
+    fi
+  '';
 }
